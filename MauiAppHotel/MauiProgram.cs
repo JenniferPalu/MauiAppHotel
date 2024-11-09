e7404a917df5c7c12f5bc3d2e761c24c40ef785d
JenniferPalu <jenniferpalulima@gmail.com>
2024-11-09 19:50:09 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiAppHotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("EBGaramond-Italic-VariableFont_wght.ttf", "GaramondItalic");
+                    fonts.AddFont("EBGaramond-VariableFont_wght.ttf", "Garamond");
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
